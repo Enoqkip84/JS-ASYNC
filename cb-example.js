fec2c71ea549401c05017819973f61d2a9c1ce91
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-07-05 16:43:34 +0800

    files

--- cb-example.js
+++ cb-example.js
@@ -3,3 +3,35 @@
 CALLBACK, PPROMISES, ASYNC/AWAIT 
 =========================================================================
 */
+
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.img-container');
+const url = 'https://images.unsplash.com/photo-1493558103817-58b2924bce98';
+
+btn.addEventListener('click', () => {
+  console.log(loadImage(url));
+
+  loadImage(url).then((data)=> console.log(data))
+});
+function loadImage(url){
+ 
+  return new Promise ((resolve, reject)=>{
+    let img = new Image();
+    img.addEventListener('load', () => {
+      resolve(img);
+    });
+    img.addEventListener('error', () => {
+      reject(new Error('Failed to load image'));
+    });
+    img.src = url;
+  });
+}
+function loadImage(url){
+  return new Promise ((resolve, reject)=>{
+    let img = new Image();
+    img.addEventListener('load', )
+  })
+}
