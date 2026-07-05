d68e21a02ef136ebead8a74cae310c4c859d45d1
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-07-05 20:18:27 +0800

    files

--- app.js
+++ app.js
@@ -188,23 +188,30 @@ CALLBACK, PPROMISES, ASYNC/AWAIT
  //@ts-check
 
 
-  const heading1 = document.querySelector('.one')
-  const heading2 = document.querySelector('.two')
-  const heading3 = document.querySelector('.three')
-  const container = document.querySelector('.img-container')
+  const heading1 = document.querySelector('.one');
+  const heading2 = document.querySelector('.two');
+  const heading3 = document.querySelector('.three');
+  const container = document.querySelector('.img-container');
   const btn = document.querySelector('.btn')
-  const url = 'https://images.unsplash.com/photo-1493558103817-58b2924bce98'
+  const url = 'https://images.unsplash.com/photo-1493558103817-58b2924bce98';
 
   btn.addEventListener('click', ()=>{
 
     
-    loadImage(url).then((data)=>console.log(data);
-    )
-  });
+    loadImage(url).then((data)=> container.appendChild(data)).catch((err)=>console.log(err));
+    });
   
 
   function loadImage(url){
     return new Promise((resolve,reject)=>{
-      
+      let img = new Image()
+      img.addEventListener('load', ()=>{
+        resolve(img)
+      });
+      img.addEventListener('error',()=>{
+        reject(new Error (`Failed to load image from the source: ${url}`));
+        
+      });
+      img.src=url;
     })
   }
